4289ed223594cdac8693b1d04df182d69e58d08f
Achyuth K T <achyuth368@gmail.com>
2024-09-05 14:46:00 +0530

    upload files

---
+++ Portfolio/script.js
@@ -0,0 +1,311 @@
+function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}function toggleMenu(){
+  const menu=document.querySelector(".menu-links")
+  const icon=document.querySelector(".menu-icon")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
